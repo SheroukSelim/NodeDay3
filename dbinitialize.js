02363806bf5581846f7bc161c18a2692b70a4814
SheroukSelim <sherouk.selim95@gmail.com>
2018-04-20 18:54:20 +0200

    fourth commit

--- dbinitialize.js
+++ dbinitialize.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 let mongo_url = 'mongodb://localhost:27017/express_sample';
-if(process.env.NODE.ENV === 'production')
+if(process.env.NODE_ENV === 'production')
 {
     mongo_url = 'mongodb://admin:admin@ds251849.mlab.com:51849/express_sample';
 }
