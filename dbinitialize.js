5d378f591e8021b45f1a5b0ae3d678f553cafe0b
SheroukSelim <sherouk.selim95@gmail.com>
2018-04-20 18:45:42 +0200

    third commit

--- dbinitialize.js
+++ dbinitialize.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/express_sample');
+
+let mongo_url = 'mongodb://localhost:27017/express_sample';
+if(process.env.NODE.ENV === 'production')
+{
+    mongo_url = 'mongodb://admin:admin@ds251849.mlab.com:51849/express_sample';
+}
+
+mongoose.connect(mongo_url);
